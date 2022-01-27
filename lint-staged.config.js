f4ca1e0b0ace05a232e3c2703710542739dfeacc
JoA-MoS <JoA-MoS@users.noreply.github.com>
2022-01-26 22:53:40 -0800

    chore: switch to json lint-staged config

--- lint-staged.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  '*': (filesArray) => {
-    const files = filesArray.join();
-    return [
-      `nx affected:lint --fix --files=${files}`,
-      `nx format:write --files=${files}`,
-    ];
-  },
-};
