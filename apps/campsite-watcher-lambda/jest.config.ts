2567a24ac2ce34c3a082073d3fceaf0033e56e0c
JoA-MoS <joamos.dev@gmail.com>
2023-04-24 20:51:16 -0700

    chore: updates nx to 15.9.2

--- apps/campsite-watcher-lambda/jest.config.ts
+++ apps/campsite-watcher-lambda/jest.config.ts
@@ -2,14 +2,15 @@
 export default {
   displayName: 'campsite-watcher-lambda',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/apps/campsite-watcher-lambda',
