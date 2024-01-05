e498cc6f4c7b28af176e38c56c2fbeec3067de24
JoA-MoS <joamos.dev@gmail.com>
2024-01-05 12:18:22 -0800

    feat: adds test docker image for nx and kaniko

---
+++ apps/nx-kaniko/release.config.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+
+const name = 'nx-kaniko';
+const pathToRepoRoot = '../..';
+
+module.exports = {
+  extends: `${pathToRepoRoot}/release.config.base.js`,
+  tagFormat: name + '-v${version}',
+  commitPaths: [
+    // REMOVING external dependencies to decrease unneeded releases and bad change logs
+    // `${pathToRepoRoot}/workspace.json`,
+    // `${pathToRepoRoot}/nx.json`,
+    // `${pathToRepoRoot}/.nxignore`,
+    // `${pathToRepoRoot}/package.json`,
+    // `${pathToRepoRoot}/.prettierrc`,
+    // `${pathToRepoRoot}/.prettierignore`,
+    `*`, // anything in this directory
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release-plus/docker',
+      {
+        name: {
+          namespace: 'joamos',
+          repository: name,
+        },
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
