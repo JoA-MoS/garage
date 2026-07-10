5149d7f11536ff803efad77bd795920696ed58b0
Justin Dietz <joamos.dev@gmail.com>
2026-07-10 13:43:38 -0700

    refactor(infra): wire App Runner, Aurora, bastion; remove ECS/ALB/logging exports

--- libs/soccer-stats/infra/src/lib/modules/index.ts
+++ libs/soccer-stats/infra/src/lib/modules/index.ts
@@ -5,12 +5,16 @@ export {
   type SecurityGroupsConfig,
   type SecurityGroupsOutputs,
 } from './security-groups';
-export { createEcsCluster, type EcsConfig, type EcsOutputs } from './ecs';
 export {
-  createLoadBalancer,
-  type LoadBalancerConfig,
-  type LoadBalancerOutputs,
-} from './load-balancer';
+  createVpcConnector,
+  type VpcConnectorConfig,
+  type VpcConnectorOutputs,
+} from './vpc-connector';
+export {
+  createBastion,
+  type BastionConfig,
+  type BastionOutputs,
+} from './bastion';
 export { createEcrRepository, type EcrConfig, type EcrOutputs } from './ecr';
 export {
   createIamRoles,
@@ -18,11 +22,6 @@ export {
   type IamConfig,
   type IamOutputs,
 } from './iam';
-export {
-  createLogGroup,
-  type LoggingConfig,
-  type LoggingOutputs,
-} from './logging';
 export {
   createDatabase,
   type DatabaseConfig,
